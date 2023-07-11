0c9013839df036923fe5a41e35631d9b72c1f406
Daria Cherniavska <dariaachernyavskaya@gmail.com>
2023-07-11 22:29:24 +0300

    UC#1 Integration

--- src/hooks/use-get-countries.js
+++ src/hooks/use-get-countries.js
@@ -1,15 +1,22 @@
 import { useState } from 'react';
 import axios from 'axios';
+import { utils } from '../utils';
 
 const baseUrl = 'https://restcountries.com/v3.1/all';
 
-const useGetCountries = () => {
+const useGetCountries = ({ name, population, sortName, pagination }) => {
   const [countries, setCountries] = useState([]);
 
   const getCountries = async () => {
     try {
       const response = await axios.get(baseUrl);
-      setCountries(response.data);
+
+      const filteredByNameData = utils.countries.filterByName(response.data, name);
+      const filteredByPopulationData = utils.countries.filterByCountryPopulation(filteredByNameData, population);
+      const sortedByNameData = utils.countries.sortByCountryName(filteredByPopulationData, sortName);
+      const paginatedData = utils.countries.paginateCountries(sortedByNameData, pagination);
+
+      setCountries(paginatedData);
     } catch (e) {
       console.log(e);
     }
--- src/pages/main/main.jsx
+++ src/pages/main/main.jsx
@@ -4,9 +4,9 @@ import { useEditFormValue, useGetCountries } from '../../hooks';
 const Main = () => {
   const [name, changeName] = useEditFormValue('');
   const [population, setPopulation] = useEditFormValue(0);
-  const [sortName, changeSortName] = useEditFormValue('');
-  const [pagination, setPagination] = useEditFormValue(0);
-  const { getCountries } = useGetCountries();
+  const [sortName, changeSortName] = useEditFormValue('ascend');
+  const [pagination, setPagination] = useEditFormValue(20);
+  const { getCountries, countries } = useGetCountries({ name, population, sortName, pagination });
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -22,10 +22,21 @@ const Main = () => {
         id='sortName'
         value={sortName}
         onChange={changeSortName}
-        label='Sort by (asc or desc, other values will be ignored)'
+        label='Sort by (ascend or descend, other values will be ignored)'
       />
       <Input id='pagination' value={pagination} onChange={setPagination} label='Paginate' type='number' />
       <button type='submit'>Submit</button>
+
+      {countries?.length ? (
+        <ul>
+          {countries.map((country) => (
+            <li key={country.name.common}>
+              <h3>{country.name.common}</h3>
+              <p>Population: {country.population}</p>
+            </li>
+          ))}
+        </ul>
+      ) : null}
     </form>
   );
 };
