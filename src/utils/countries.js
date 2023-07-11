cdcf341562004ab1e4ac17ec49faf5a7ae5e1857
Daria Cherniavska <dariaachernyavskaya@gmail.com>
2023-07-11 22:13:24 +0300

    UC#1 Create filter by population function

--- src/utils/countries.js
+++ src/utils/countries.js
@@ -4,4 +4,10 @@ const filterByName = (countries, name) => {
   return countries.filter((country) => country.name.common.toLowerCase().includes(cleanName));
 };
 
-export const countries = { filterByName };
+export const filterByCountryPopulation = (countries, populationInMillions) => {
+  const population = populationInMillions * 1000000;
+
+  return countries.filter((country) => country.population <= population);
+};
+
+export const countries = { filterByName, filterByCountryPopulation };
